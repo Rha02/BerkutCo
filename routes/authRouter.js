2871870f70c7dbebdb45f507ae3b62fe7df8fc09
Romuz Abdulhamidov <52812369+Rha02@users.noreply.github.com>
2022-11-13 17:30:48 -0800

    Upgraded Get Products route + Improved HTTP Status and Error responses (#4)
    * Installed express-validator and Built Authentication Validation
    
    * Created Product Model and Product Validation Schema + Tweaked User Model
    
    * Created CRUD operations for Products
    
    * Updated Get Products Route to Return Latest Products + Added Support to Skip and Limit Products response
    
    * Created a Module for HTTP Status Codes
    
    * Improved error responses

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,11 +5,13 @@ const bcrypt = require("bcrypt")
 const jwt = require('jsonwebtoken')
 const { validationResult, checkSchema } = require("express-validator")
 const {loginSchema, registerSchema} = require('../middleware/authSchema')
+const http = require("../utils/http")
+
 
 router.post("/login", checkSchema(loginSchema), async (req, res) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-        return res.status(400).json({errors: errors.array()})
+        return res.status(http.statusBadRequest).json({ errors: errors.array() })
     }
 
     try {
@@ -18,37 +20,49 @@ router.post("/login", checkSchema(loginSchema), async (req, res) => {
         })
 
         if (!user) {
-            return res.status(400).json("Error: User does not exist")
+            return res.status(http.statusNotFound).json({
+                errors: [{ msg: "User not found" }]
+            })
         }
 
         passwordsMatch = await bcrypt.compare(req.body.password, user.password)
         if (!passwordsMatch) {
-            return res.status(401).json("Error: Invalid credentials")
+            return res.status(http.statusUnauthorized).json({
+                errors: [{ msg: "Invalid credentials" }]
+            })
         }
 
         const token = jwt.sign({ _id: user._id }, process.env.SECRET_TOKEN)
-
-        res.setHeader("Authorization", token).json(token)
+        
+        res.setHeader("Authorization", token).json({
+            errors: []
+        })
     } catch(err) {
-        res.status(500).json("Error: Unexpected error encountered")
+        res.status(http.statusInternalServerError).json({
+            errors: [{ msg: "Unexpected error encountered" }]
+        })
     }
 })
 
 router.post("/register", checkSchema(registerSchema), async (req, res) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array())
+        return res.status(http.statusBadRequest).json(errors.array())
     }
 
     try {
         const emailExists = await User.findOne({ email: req.body.email })
         if (emailExists) {
-            return res.status(400).json("Error: User with this email already exists")
+            return res.status(http.statusBadRequest).json({
+                errors: [{ msg: "Email already in use" }]
+            })
         }
 
         const usernameExists = await User.findOne({ username: req.body.username })
         if (usernameExists) {
-            return res.status(400).json("Error: User with this username already exists")
+            return res.status(http.statusBadRequest).json({
+                errors: [{ msg: "Username already in use" }]
+            })
         }
 
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
@@ -59,16 +73,20 @@ router.post("/register", checkSchema(registerSchema), async (req, res) => {
         })
 
         const savedUser = await user.save()
-        res.status(201).json({ user_id: savedUser._id })
+        return res.status(http.statusCreated).json({ user_id: savedUser._id, errors: [] })
     } catch (err) {
-        res.status(500).json("Error: Unexpected error encountered")
+        return res.status(http.statusInternalServerError).json({
+            errors: [{ msg: "Unexpected error encountered" }]
+        })
     }
 })
 
 router.get("/checkauth", async (req, res) => {
     const token = req.header('Authorization')
     if (!token) {
-        return res.status(401).json("Error: Unauthenticated")
+        return res.status(http.statusUnauthorized).json({
+            errors: [{ msg: "Unauthenticated" }]
+        })
     }
 
     console.log(token)
@@ -77,18 +95,28 @@ router.get("/checkauth", async (req, res) => {
     try {
         u = jwt.verify(token, process.env.SECRET_TOKEN)
     } catch(err) {
-        return res.status(401).json("Error: Invalid Authentication Token")
-    }
-    
-    const user = await User.findById({_id: u._id})
-    if (!user) {
-        return res.status(404).json("Error: User not found")
+        return res.status(http.statusUnauthorized).json({
+            errors: [{ msg: "Invalid authentication token" }]
+        })
     }
 
-    res.json({
-        email: user.email,
-        username: user.username
-    })
+    try {
+        const user = await User.findById({ _id: u._id })
+        if (!user) {
+            return res.status(http.statusNotFound).json({
+                errors: [{ msg: "Invalid authentication token" }]
+            })
+        }
+
+        return res.json({
+            email: user.email,
+            username: user.username
+        })
+    } catch(err) {
+        return res.status(http.statusInternalServerError).json({
+            errors: [{ msg: "Unexpected error encountered" }]
+        })
+    }
 })
 
 module.exports = router
---
+++ utils/http.js
@@ -0,0 +1,11 @@
+const httpResponses = {
+    statusOK: 200,
+    statusCreated: 201,
+    statusBadRequest: 400,
+    statusUnauthorized: 401,
+    statusForbidden: 403,
+    statusNotFound: 404,
+    statusInternalServerError: 500,
+    statusNotImplemented: 501
+}
+module.exports = httpResponses
