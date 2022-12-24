caff45d506b64cd6940b37bb2f752fdf49dd3aa4
Romuz Abdulhamidov <52812369+Rha02@users.noreply.github.com>
2022-12-24 14:15:16 -0800

    Merging API tests to main (#11)
    * Refactored codebase for future testing
    
    * Created tests for Auth API
    
    * Added tests to Products API

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,10 +1,156 @@
 const app = require('../app')
+const User = require('../models/user')
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const request = require('supertest')
+const http = require('../utils/http')
 
-beforeEach(async () => {
+beforeAll(async () => {
     await mongoose.connect(process.env.TEST_DATABASE_URL)
+        .catch(err => {
+            console.error(`Error connecting to MongoDB: ${err}`)
+            process.exitCode = 1
+        })
+    // Create a test user
+    const hashedPassword = await bcrypt.hash("fake-password", 10)
+    const user = new User({
+        email: "fake@email.test",
+        username: "testuser",
+        password: hashedPassword
+    })
+    await user.save()
 })
 
-afterEach(async () => {
+afterAll(async () => {
+    // Delete the test user
+    await User.deleteOne({ email: "fake@email.test" })
     await mongoose.connection.close()
+})
+
+describe("POST /login", () => {
+    test("should successfuly login", async () => {
+        const res = await request(app).post("/login").expect("Content-Type", /json/).send({
+            email: "fake@email.test",
+            password: "fake-password"
+        })
+        expect(res.statusCode).toBe(http.statusOK)
+        expect(res.headers["authorization"]).toBeDefined()
+        expect(res.body["errors"]).toHaveLength(0)
+    })
+
+    test("user email should not be found", async () => {
+        const res = await request(app).post("/login").expect("Content-Type", /json/).send({
+            email: "doesnotexit@email.test",
+            password: "fake-password"
+        })
+
+        expect(res.statusCode).toBe(http.statusNotFound)
+        expect(res.headers["authorization"]).toBeUndefined()
+        expect(res.body["errors"]).toHaveLength(1)
+    })
+
+    test("password should be incorrect", async () => {
+        const res = await request(app).post("/login").expect("Content-Type", /json/).send({
+            email: "fake@email.test",
+            password: "wrong-password"
+        })
+
+        expect(res.statusCode).toBe(http.statusUnauthorized)
+        expect(res.headers["authorization"]).toBeUndefined()
+        expect(res.body["errors"]).toHaveLength(1)
+    })
+})
+
+describe("POST /register", () => {
+    test("should successfully register", async () => {
+        const res = await request(app).post("/register").expect("Content-Type", /json/).send({
+            email: "john@email.loc",
+            username: "JohnDoe",
+            password: "TheJohnDoe"
+        })
+
+        expect(res.statusCode).toBe(http.statusCreated)
+        expect(res.body["user_id"]).toBeDefined()
+
+        await User.deleteOne({_id: res.body["user_id"]})
+    })
+
+    test("email in wrong email format", async () => {
+        const res = await request(app).post("/register").expect("Content-Type", /json/).send({
+            email: "not an email",
+            username: "JohnDoe",
+            password: "TheJohnDoe"
+        })
+
+        expect(res.statusCode).toBe(http.statusBadRequest)
+    })
+
+    test("email already be in use", async () => {
+        const res = await request(app).post("/register").expect("Content-Type", /json/).send({
+            email: "fake@email.test",
+            username: "JohnDoe",
+            password: "TheJohnDoe"
+        })
+
+        expect(res.statusCode).toBe(http.statusBadRequest)
+        expect(res.body["errors"]).toHaveLength(1)
+    })
+
+    test("username already be in use", async () => {
+        const res = await request(app).post("/register").expect("Content-Type", /json/).send({
+            email: "john@email.loc",
+            username: "testuser",
+            password: "TheJohnDoe"
+        })
+
+        expect(res.statusCode).toBe(http.statusBadRequest)
+        expect(res.body["errors"]).toHaveLength(1)
+    })
+
+    test("password less than 8 characters", async () => {
+        const res = await request(app).post("/register").expect("Content-Type", /json/).send({
+            email: "john@email.loc",
+            username: "JohnDoe",
+            password: "12345"
+        })
+
+        expect(res.statusCode).toBe(http.statusBadRequest)
+    })
+
+    test("username less than 6 characters", async () => {
+        const res = await request(app).post("/register").expect("Content-Type", /json/).send({
+            email: "john@email.loc",
+            username: "12345",
+            password: "TheJohnDoe"
+        })
+
+        expect(res.statusCode).toBe(http.statusBadRequest)
+    })
+})
+
+describe("GET /checkauth", () => {
+    test("should successfully get current user", async () => {
+        const res1 = await request(app).post("/login").send({
+            email: "fake@email.test",
+            password: "fake-password"
+        })
+        expect(res1.statusCode).toBe(http.statusOK)
+
+        const res2 = await request(app).get("/checkauth").set({"Authorization": res1.headers["authorization"]})
+        expect(res2.statusCode).toBe(http.statusOK)
+        expect(res2.body["email"]).toBe("fake@email.test")
+        expect(res2.body["username"]).toBe("testuser")
+    })
+
+    test("missing authorization token", async () => {
+        const res = await request(app).get("/checkauth")
+        expect(http.statusBadRequest)
+        expect(res.body["errors"]).toHaveLength(1)
+    })
+
+    test("invalid authorization token", async () => {
+        const res = await request(app).get("/checkauth").set({"Authorization": "invalid token here"})
+        expect(http.statusBadRequest)
+        expect(res.body["errors"]).toHaveLength(1)
+    })
 })
---
+++ tests/product.test.js
@@ -0,0 +1,308 @@
+const app = require('../app')
+const User = require('../models/user')
+const Product = require('../models/product')
+const mongoose = require('mongoose')
+const request = require('supertest')
+const http = require('../utils/http')
+const bcrypt = require('bcrypt')
+
+beforeAll(async () => {
+    await mongoose.connect(process.env.TEST_DATABASE_URL)
+        .catch(err => {
+            console.error(`Error connecting to MongoDB: ${err}`)
+            process.exitCode = 1
+        })
+
+    // Create a test user
+    const hp1 = await bcrypt.hash("fake-password", 10)
+    const user = new User({
+        email: "john@foo.bar",
+        username: "john",
+        password: hp1
+    })
+    await user.save()
+
+    // Create a second test user
+    const hp2 = await bcrypt.hash("unauthorized", 10)
+    const u = new User({
+        email: "unauthorizeduser@email.loc",
+        username: "unauthorizeduser",
+        password: hp2
+    })
+    await u.save()
+
+    // Create dummy products
+    const product = new Product({
+        name: "product 1",
+        description: "this is product 1",
+        price: 10.49,
+        seller: user._id
+    })
+    await product.save()
+
+    const product2 = new Product({
+        name: "product 2",
+        description: "this is product 2",
+        price: 5.73,
+        seller: user._id
+    })
+    await product2.save()
+
+    const product3 = new Product({
+        name: "product 3",
+        description: "this is product 3",
+        price: 13.21,
+        seller: user._id
+    })
+    await product3.save()
+})
+
+afterAll(async () => {
+    // Delete the dummy user and products
+    const user = await User.findOne({ email: "john@foo.bar" })
+    await Product.deleteMany({ seller: user._id})
+    await user.delete()
+    await User.deleteOne({ username: "unauthorizeduser" })
+    await mongoose.connection.close()
+})
+
+describe("GET /products", () => {
+    test("should get products", async () => {
+        const res = await request(app).get("/products").expect("Content-Type", /json/)
+        expect(res.statusCode).toBe(http.statusOK)
+        expect(res.body).toHaveLength(3)
+        expect(res.body[0].name).toBe("product 3")
+    })
+    
+    test("should get products but skip first product", async () => {
+        const res = await request(app).get("/products?skip=1").expect("Content-Type", /json/)
+        expect(res.statusCode).toBe(http.statusOK)
+        expect(res.body).toHaveLength(2)
+        expect(res.body[0].name).toBe("product 2")
+        expect(res.body[1].name).toBe("product 1")
+    })
+
+    test("should get products but limited to 2 products", async () => {
+        const res = await request(app).get("/products?limit=2").expect("Content-Type", /json/)
+        expect(res.statusCode).toBe(http.statusOK)
+        expect(res.body).toHaveLength(2)
+        expect(res.body[0].name).toBe("product 3")
+        expect(res.body[1].name).toBe("product 2")
+    })
+
+    test("should get products but skipped 1 product and limited to 1 product", async () => {
+        const res = await request(app).get("/products?limit=1&skip=1").expect("Content-Type", /json/)
+        expect(res.statusCode).toBe(http.statusOK)
+        expect(res.body).toHaveLength(1)
+        expect(res.body[0].name).toBe("product 2")
+    })
+})
+
+describe("GET /products/:id", () => {
+    test("should get product", async () => {
+        const product = await Product.findOne({ name: "product 1" })
+        const res = await request(app).get(`/products/${product._id}`)
+        expect(res.statusCode).toBe(http.statusOK)
+        expect(res.body["description"]).toBe(product["description"])
+        expect(res.body["_id"]).toBe(product._id.toString())
+    })
+
+    test("should not find product", async () => {
+        productId = mongoose.Types.ObjectId();
+        const res = await request(app).get(`/products/${productId}`)
+        expect(res.statusCode).toBe(http.statusNotFound)
+        expect(res.body["_id"]).toBeUndefined()
+    })
+})
+
+describe("POST /products", () => {
+    test("should create a product", async () => {
+        const u = await request(app).post("/login").send({
+            email: "john@foo.bar",
+            password: "fake-password"
+        })
+
+        const res = await request(app).post("/products").set({"Authorization": u.headers["authorization"]}).send({
+            name: "product 4",
+            description: "this is product 4",
+            price: 20
+        })
+        expect(res.statusCode).toBe(http.statusCreated)
+        expect(res.body["_id"]).toBeDefined()
+
+        await Product.deleteOne({"_id": res.body["_id"]})
+    })
+
+    test("unauthenticated should fail to create a product", async () => {
+        const res = await request(app).post("/products").send({
+            name: "product 4",
+            description: "this is product 4",
+            price: 20
+        })
+        expect(res.statusCode).toBe(http.statusUnauthorized)
+        expect(res.body["_id"]).toBeUndefined()
+    })
+
+    test("product name should be at least 5 characters long", async () => {
+        const u = await request(app).post("/login").send({
+            email: "john@foo.bar",
+            password: "fake-password"
+        })
+
+        const res = await request(app).post("/products").set({ "Authorization": u.headers["authorization"] }).send({
+            name: "123",
+            description: "this is product 4",
+            price: 20
+        })
+        expect(res.statusCode).toBe(http.statusBadRequest)
+        expect(res.body["_id"]).toBeUndefined()
+    })
+
+    test("product price should not be negative", async () => {
+        const u = await request(app).post("/login").send({
+            email: "john@foo.bar",
+            password: "fake-password"
+        })
+
+        const res = await request(app).post("/products").set({ "Authorization": u.headers["authorization"] }).send({
+            name: "product 4",
+            description: "this is product 4",
+            price: -1
+        })
+        expect(res.statusCode).toBe(http.statusBadRequest)
+        expect(res.body["_id"]).toBeUndefined()
+    })
+})
+
+describe("PUT /products/:id", () => {
+    test("should successfully update a product", async () => {
+        const u = await request(app).post("/login").send({
+            email: "john@foo.bar",
+            password: "fake-password"
+        })
+        expect(u.statusCode).toBe(http.statusOK)
+
+        const product = await Product.findOne({ name: "product 1" })
+        const res = await request(app).put(`/products/${product._id}`).set({ "Authorization": u.headers["authorization"] }).send({
+            name: "updated product 1",
+            description: "updated description",
+            price: 20.59
+        })
+
+        expect(res.statusCode).toBe(http.statusOK)
+        expect(res.body["name"]).toBe("updated product 1")
+        expect(res.body["description"]).toBe("updated description")
+        expect(res.body["price"]).toBe(20.59)
+    })
+
+    test("unauthorized user should fail to update a product", async () => {
+        const u = await request(app).post("/login").send({
+            email: "unauthorizeduser@email.loc",
+            password: "unauthorized"
+        })
+        expect(u.statusCode).toBe(http.statusOK)
+
+        const product = await Product.findOne({ name: "product 2" })
+        const res = await request(app).put(`/products/${product._id}`).set({ "Authorization": u.headers["authorization"]}).send({
+            name: "updated product 2",
+            description: "updated description",
+            price: 20.59
+        })
+
+        expect(res.statusCode).toBe(http.statusForbidden)
+        expect(res.body["name"]).toBeUndefined()
+    })
+
+    test("product name should be at least 5 characters long", async () => {
+        const u = await request(app).post("/login").send({
+            email: "john@foo.bar",
+            password: "fake-password"
+        })
+        expect(u.statusCode).toBe(http.statusOK)
+
+        const product = await Product.findOne({ name: "product 2" })
+        const res = await request(app).put(`/products/${product._id}`).set({ "Authorization": u.headers["authorization"] }).send({
+            name: "123",
+            description: "updated description",
+            price: 20.59
+        })
+
+        expect(res.statusCode).toBe(http.statusBadRequest)
+        expect(res.body["name"]).toBeUndefined()
+    })
+
+    test("product price should not be negative", async () => {
+        const u = await request(app).post("/login").send({
+            email: "john@foo.bar",
+            password: "fake-password"
+        })
+        expect(u.statusCode).toBe(http.statusOK)
+
+        const product = await Product.findOne({ name: "product 2" })
+        const res = await request(app).put(`/products/${product._id}`).set({ "Authorization": u.headers["authorization"] }).send({
+            name: "12345",
+            description: "updated description",
+            price: -5
+        })
+
+        expect(res.statusCode).toBe(http.statusBadRequest)
+        expect(res.body["name"]).toBeUndefined()
+    })
+
+    test("should not find product", async () => {
+        const u = await request(app).post("/login").send({
+            email: "john@foo.bar",
+            password: "fake-password"
+        })
+        expect(u.statusCode).toBe(http.statusOK)
+
+        const id = mongoose.Types.ObjectId()
+        const res = await request(app).put(`/products/${id}`).set({ "Authorization": u.headers["authorization"] }).send({
+            name: "12345",
+            description: "updated description",
+            price: 20.59
+        })
+
+        expect(res.statusCode).toBe(http.statusNotFound)
+        expect(res.body["name"]).toBeUndefined()
+    })
+})
+
+describe("DELETE /products/:id", () => {
+    test("should delete product", async () => {
+        const u = await request(app).post("/login").send({
+            email: "john@foo.bar",
+            password: "fake-password"
+        })
+        expect(u.statusCode).toBe(http.statusOK)
+        
+        const product = await Product.findOne({ name: "product 3" })
+        const res = await request(app).delete(`/products/${product._id}`).set({ "Authorization": u.headers["authorization"] })
+        expect(res.statusCode).toBe(http.statusOK)
+        expect(await Product.findOne({ _id: product._id })).toBeNull()
+    })
+
+    test("unauthorized user should fail to delete product", async () => {
+        const u = await request(app).post("/login").send({
+            email: "unauthorizeduser@email.loc",
+            password: "unauthorized"
+        })
+        expect(u.statusCode).toBe(http.statusOK)
+
+        const product = await Product.findOne({ name: "product 2" })
+        const res = await request(app).delete(`/products/${product._id}`).set({ "Authorization": u.headers["authorization"] })
+        expect(res.statusCode).toBe(http.statusForbidden)
+    })
+
+    test("should not find product to delete", async () => {
+        const u = await request(app).post("/login").send({
+            email: "john@foo.bar",
+            password: "fake-password"
+        })
+        expect(u.statusCode).toBe(http.statusOK)
+
+        const id = mongoose.Types.ObjectId()
+        const res = await request(app).delete(`/products/${id}`).set({ "Authorization": u.headers["authorization"] })
+        expect(res.statusCode).toBe(http.statusNotFound)
+    })
+})
