a8f20d2290d41eb2ee67dda8eafd97a9451e0e31
Rha02 <romuzus08@gmail.com>
2023-01-17 11:16:07 -0800

    Should successfully run tests

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -91,8 +91,6 @@ router.post("/register", checkSchema(registerSchema), async (req, res) => {
 router.get("/checkauth", requiresAuthentication, async (req, res) => {
     const user = req.user
     user.password = undefined
-
-    return res.json("SHOULD FAIL")
     return res.json(user)
 })
 
